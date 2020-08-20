0727c776212add8d79182a9f12cab75a57a93d64
ClareP212 <56163311+ClareP212@users.noreply.github.com>
2020-08-20 23:40:41 +0100

    Update gt_process.py

--- gt_process.py
+++ gt_process.py
@@ -1,6 +1,5 @@
 """
 Created on Mon May 25 19:20:00 2020
-
 @author: Clare
 
 Code from https://opensourceoptions.com
@@ -12,9 +11,11 @@ import geopandas as gpd
 import pandas as pd
 
 # read ground truth points shapefile to geopandas geodataframe
-gd_f = gpd.read_file ('C:\\Users\\clare\\MscDiss\\GT\\WB_GT.shp')
+gd_f = gpd.read_file ('C:\\Users\\Clare\\Documents\\MscDiss\\GT\\RW_GT.shp')
+
 # get names of land cover classes/labels
 class_names = gd_f['lc'].unique()
+class_names = np.sort(class_names)  # alphabetise class names to bog, forest, grass
 print('class names', class_names)
 
 # raster cant hold string, so have to create corresponding id number
@@ -24,16 +25,14 @@ print('class ids', class_ids)
 
 # create a pandas data frame of the labels and ids and save to csv
 df = pd.DataFrame({'lc': class_names, 'id': class_ids})
-df.to_csv('C:\\Users\\Clare\\MscDiss\\GT\\WB_class_lookup.csv')
 print('gd_f without ids',gd_f.head())
 # add a new column to geodatafame with the id for each class/label
 gd_f['id']= gd_f['lc'].map(dict(zip(class_names,class_ids)))
 print('gd_f with ids',gd_f.head())
 
-
 # split into training and test subsets
-gdf_train = gd_f.sample(frac=0.7)
+gdf_train = gd_f.sample(frac=0.7)  # 70% training
 gdf_test = gd_f.drop(gdf_train.index)
 print('gd_f shape', gd_f.shape,'training shape', gdf_train.shape, 'test', gdf_test.shape)
-gdf_train.to_file('C:\\Users\\Clare\\MscDiss\\GT\\WB_gt_train.shp')
-gdf_test.to_file('C:\\Users\\Clare\\MscDiss\\GT\\WB_gt_test.shp')
+gdf_train.to_file('C:\\Users\\Clare\\Documents\\MscDiss\\GT\\RW_gt_train.shp')
+gdf_test.to_file('C:\\Users\\Clare\\Documents\\MscDiss\\GT\\RW_gt_test.shp')
